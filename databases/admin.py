fcf4e534ffe9549888931ce080fb04b679043eb5
nivid-koradiya <ndkoradiya@gmail.com>
2023-02-05 22:32:07 +0530

    Models for loginlog, client, clientadmin created

--- databases/admin.py
+++ databases/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import (
+    LoginLog,
+    ClientAdmin,
+    Client
+)
+
+class LoginLogAdmin(admin.ModelAdmin):
+    readonly_fields=[
+        # 'id','timestamp'
+    ]
+admin.site.register(LoginLog,LoginLogAdmin)
+
+class ClientAdminAdmin(admin.ModelAdmin):
+    readonly_fields=[
+        # 'id','timestamp'
+    ]
+admin.site.register(ClientAdmin,ClientAdminAdmin)
+
+
+class ClientAdmin(admin.ModelAdmin):
+    readonly_fields=[
+        # 'id','timestamp'
+    ]
+admin.site.register(Client,ClientAdmin)
--- databases/models.py
+++ databases/models.py
@@ -1,3 +1,105 @@
 from django.db import models
-
+import uuid
+import re
+import datetime as dt
+from django.forms import ValidationError
+from django.contrib.auth.hashers import make_password
 # Create your models here.
+
+
+#GENERAL FUNCTIONS
+def generate_uuid():
+    return str(uuid.uuid4()) #RETURN UUID v4 in string format
+
+
+
+
+
+
+#logging Functions:
+#Logging Models
+class LoginLog(models.Model):
+    id =  models.CharField(default=generate_uuid, primary_key=True,max_length=42)
+    #TODO Add user field here
+    timestamp = models.DateTimeField(auto_now_add=True)
+    #choices for Mode of login
+    mode_of_login_choices = (
+        ('browser','browser'),
+        ('other','other'),
+        # ('',''),
+    )
+    mode_of_login = models.CharField(max_length=100,choices=mode_of_login_choices,default='browser')
+    class Meta:
+        verbose_name = ("login_log")
+        verbose_name_plural = ("login_logs")
+        db_table = "log_login"
+        
+    def __str__(self):
+        return self.id
+
+
+
+
+
+#ORG/CLIENT USER FUNCTIONS
+def client_mobile_validator(mobile):
+    mobile= str(mobile)
+    if mobile.isnumeric() and len(mobile)==10:
+        return True
+    else:
+        raise ValidationError("Mobile No is not valid")
+        
+#ORG/CLIENT USER MODELS
+class Client(models.Model):
+    id = models.CharField(default=generate_uuid, primary_key=True,max_length=42)
+    organisation = models.CharField(max_length=255)
+    # admin = models.CharField() #TODO #ADD ADMIN TABLE USER HERE
+    mobile = models.CharField(max_length=10,validators=[])
+    timestamp = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = ("client")
+        verbose_name_plural = ("clients")
+        db_table = "clients"
+        
+    def __str__(self):
+        return self.organisation
+
+
+
+
+
+
+# Client Admin functions
+def validate_username(username):
+    username_len = len(username)
+    username = str(username)
+    if username_len >=8 and username_len<33 and re.match(r'^\w+$', username):
+        return True
+    else:
+        raise ValidationError("Username is not Valid")
+
+def generate_default_password_hash():
+    hashed_password = ""
+    hashed_password = make_password('admin')
+    return hashed_password
+# Client Admin:
+class ClientAdmin(models.Model):
+    id = models.CharField(default=generate_uuid, primary_key=True,max_length=42)
+    name = models.CharField(max_length=125,null=False,default='default')
+    username = models.CharField(max_length=64,validators=[validate_username],null=False)
+    email = models.EmailField(max_length=254,null=False,default='defaultmail@mail.com')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    password = models.CharField(max_length=1280,null=False,default=generate_default_password_hash)
+    client = models.ForeignKey('Client',on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = ("ClientAdmin")
+        verbose_name_plural = ("ClientAdmins")
+        db_table= "client_admin"
+    def __str__(self):
+        return self.username
+
+
+
