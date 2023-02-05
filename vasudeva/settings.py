be2de6075b9e6a2d36a6cfd9a653e91c40534a94
nivid-koradiya <ndkoradiya@gmail.com>
2023-02-05 14:23:24 +0530

    Added custom apps to installed apps

--- vasudeva/settings.py
+++ vasudeva/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #CUSTOM APPS
+    'databases',
+    'api',
+    'frontend'
 ]
 
 MIDDLEWARE = [
