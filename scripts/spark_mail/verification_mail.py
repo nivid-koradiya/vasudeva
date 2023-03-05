17c8da646a84bf48119cac504cc4f1b9a89d4431
nivid-koradiya <ndkoradiya@gmail.com>
2023-03-05 11:17:01 +0530

    verification mail scripted done

---
+++ scripts/spark_mail/verification_mail.py
@@ -0,0 +1,30 @@
+import smtplib
+from email.mime.text import MIMEText
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+def send_email(subject,recipients,data):
+    password = env('MAIL_PASSWORD')
+    sender = env("MAIL_ID")
+    body=''
+    with open('scripts/spark_mail/templates/verification_mail.html','r') as f:
+        body = f.read()
+        print(body)
+    try:
+        body = body.replace('**id**',data['id'])
+    except:
+        pass
+    try:    
+        body = body.replace('**url**',data['url'])
+    except:
+        pass
+    msg = MIMEText(body,'html')
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+    smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    smtp_server.login(sender, password)
+    smtp_server.sendmail(sender, recipients, msg.as_string())
+    smtp_server.quit()
